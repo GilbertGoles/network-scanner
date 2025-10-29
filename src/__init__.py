b092a28827e9b23a419c58a84f55a27828db5f8b
Bot <dikov39@gmail.com>
2025-10-29 19:53:28 +0000

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,7 @@
 """
-Advanced Network Scanner Package
-Модуль для профессионального анализа сетей и уязвимостей
+Network Scanner Package
+Модуль для сканирования и визуализации WiFi сетей
 """
 
-__version__ = "2.0.0"
+__version__ = "1.0.0"
 __author__ = "BotPany"
-__description__ = "Network Security Scanner"
